06fad2a95dec712d889a82a8d70ed75ac3aed7a0
Hafizh24 <hafizhcbs@gmail.com>
2023-08-17 23:35:29 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import { Footer, Form, GroceryList, Header } from "./index";
+
+const groceryItems = [
+  {
+    id: 1,
+    name: "Kopi Bubuk",
+    quantity: 2,
+    checked: true,
+  },
+  {
+    id: 2,
+    name: "Gula Pasir",
+    quantity: 5,
+    checked: false,
+  },
+  {
+    id: 3,
+    name: "Air Mineral",
+    quantity: 3,
+    checked: false,
+  },
+];
+
+export default function App() {
+  const [items, setItems] = useState(groceryItems);
+
+  function handleAddItem(item) {
+    setItems([...items, item]);
+  }
+
+  function hadleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToogleItem(id) {
+    setItems((items) =>
+      items.map((item) => (item.id === id ? { ...item, checked: !item.checked } : item))
+    );
+  }
+
+  function handleClerItems() {
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Header />
+      <Form onAddItem={handleAddItem} />
+      <GroceryList
+        items={items}
+        onDeleteItem={hadleDeleteItem}
+        onToggleItem={handleToogleItem}
+        onClearItems={handleClerItems}
+      />
+      <Footer items={items} />
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from "./components/Footer";
+export { default as Form } from "./components/Form";
+export { default as GroceryList } from "./components/GroceryList";
+export { default as Header } from "./components/Header";
+export { default as Item } from "./components/Item";
